ba601301d98d56314f9a61de63284ad02a8b94db
rishulayerfive <61156797+rishulayerfive@users.noreply.github.com>
2024-07-23 13:34:44 +0530

    Fix: db creation

--- main.py
+++ main.py
@@ -1,42 +1,24 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, redirect, url_for
-from flask import render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Email, Length
-from flask_sqlalchemy import SQLAlchemy
 
-
-
-# create the app
+# Create the app
 app = Flask(__name__)
-# configure the SQLite database, relative to the app instance folder
+# Configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# create the extension
-db = SQLAlchemy(app)
-# initialize the app with the extension
-# db.init_app(app)
 app.config["WTF_CSRF_ENABLED"] = False
-# app.config["SECRET_KEY"] = "my_secret_key_1234567890"
+db = SQLAlchemy(app)
 
-
-# # app.debug = True
-
-
-with app.app_context():
-    db.create_all()
-
-
-
-# models
+# Models
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     phone = db.Column(db.Integer, nullable=False)
     password = db.Column(db.String(8), nullable=False)
-
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -49,7 +31,6 @@ class Product(db.Model):
     def __repr__(self):
         return f"Product('{self.name}', '{self.price}')"
 
-
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -61,6 +42,16 @@ class Cart(db.Model):
     def __repr__(self):
         return f"Cart('{self.user.username}')"
 
+# Order model
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('orders', lazy=True))
+    order_date = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    total_amount = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"Order('{self.id}', '{self.user.username}', '{self.total_amount}')"
 
 class Billing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -72,11 +63,14 @@ class Billing(db.Model):
     def __repr__(self):
         return f"Billing('{self.order.id}', '{self.amount}')"
 
+# Category model
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
 
-
-
-
-
+    def __repr__(self):
+        return f'<Category {self.name}>'
 
 # Flask Forms using FlaskForm and WTForms
 
@@ -86,7 +80,6 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=4)])
     remember = BooleanField('Remember Me')
 
-
 # Define RegisterForm
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=10)])
@@ -94,104 +87,78 @@ class RegisterForm(FlaskForm):
     phone = IntegerField('Phone', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=10)])
 
-
-
-
-
-
-
-
 # Routes and handle form submission
 
 # Route to render login page and handle form submission
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    
-        # Retrieve data from the form
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
         remember = form.remember.data
-        
+
         user = User.query.filter_by(username=username).first()
         if user and password == user.password:
             if remember:
-                # session['username'] = username
                 pass
-            return redirect(url_for('/'))
+            return redirect(url_for('index'))
         else:
             return redirect(url_for('login'))
-    
+
     return render_template('login.html', form=form)
-
-
-
 
 # Route to render register page and handle form submission
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-    print('register')
-    
     if form.validate_on_submit():
         # Retrieve data from the form
         username = form.username.data
         email = form.email.data
         phone = form.phone.data
         password = form.password.data
-        print('username:', username)
+
         # Create a new user instance
         new_user = User(username=username, email=email, phone=phone, password=password)
-        print(new_user)
         
         # Add new user to the database
         db.session.add(new_user)
         db.session.commit()
 
-        print("successfully created a new user")
         # Redirect to login page after successful registration
         return redirect(url_for('login'))
     
     # Render the register page template with form
     return render_template('register.html', form=form)
 
-
 @app.route('/about')
 def about():
     return render_template("about.html")
-
 
 @app.route('/cart')
 def cart():
     return render_template('cart.html')
 
-
 @app.route('/checkout')
 def checkout():
     return render_template('checkout.html')
-
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-
 @app.route('/shop')
 def shop():
     return render_template('shop.html')
-
 
 @app.route('/single_product')
 def single_product():
     return render_template('single-product.html')
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
 
 @app.route('/admin')
 def admin():
@@ -201,10 +168,7 @@ def admin():
 def list_orders():
     return render_template('list_orders.html')
 
-
-
-
-
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run()
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
